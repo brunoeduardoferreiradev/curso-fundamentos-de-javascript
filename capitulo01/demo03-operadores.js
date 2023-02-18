fa15b2e499acecb75a4f92736512345cfbf22ffe
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-02-18 18:59:31 -0300

    Aula :  Demonstração: Conhecendo os operadores do Javascript

---
+++ capitulo01/demo03-operadores.js
@@ -0,0 +1,38 @@
+// Aula :  Demonstração: Conhecendo os operadores do Javascript
+// Soma
+console.log("1 + 1 é    :", 1 + 1);
+
+// Subtração
+console.log("2 - 4 é    :", 2 - 4);
+
+//Multiplicação 
+console.log("2 * 6 é    :", 2 * 6);
+
+// Divisão
+console.log("2 / 4 é    :", 2 / 4);
+
+// Resto Módulo
+console.log("2 % 4 é    :", 2 % 4);
+
+// Exponenciação
+console.log("2 ** 4 é   :", 2 ** 4);
+
+console.log("\n--- Operadores ---\n")
+
+// Maior que
+console.log("1 > 2 é    :", 1 > 2);
+
+// Menor que
+console.log("2 < 4  é   :", 2 < 4);
+
+// Igual a
+console.log("1 === 2 é  :", 1 === 2);
+
+// Diferente de 
+console.log("1 !== 2 é  :", 1 !== 2);
+
+// Maior ou igual a 
+console.log("1 >= 1 é   :", 1 >= 1);
+
+// Menor ou igual a 
+console.log("2 <= 1 é   :", 2 <= 1);
