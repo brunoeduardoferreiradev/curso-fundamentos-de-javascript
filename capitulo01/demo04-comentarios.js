408fb29bb63820f86be4c2c8cd9725e4018f7977
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-02-18 19:05:15 -0300

    Aula : Comentando código

---
+++ capitulo01/demo04-comentarios.js
@@ -0,0 +1,13 @@
+// Aula : Comentando código
+
+// Comentário em uma única linha
+
+/**
+ * Esse código é Hackerzao
+ * Author : Erick Wendel
+ * Data: 10/01/2020
+ */
+
+// ignorando a linha abaixo
+// const variavelConstante = 1+2+4+4
+const variavelConstante = 1 + 4 // comentario na linha
