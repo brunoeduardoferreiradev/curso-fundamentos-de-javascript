6307bd1acf65af840714ec306242715a39369af9
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-02-18 18:22:05 -0300

    Aula : Demonstração: Guardando informações em Variáveis

---
+++ capitulo01/demo01-variáveis.js
@@ -0,0 +1,14 @@
+// Aula : Demonstração: Guardando informações em Variáveis
+let nome = "Super man";
+let idade = 100;
+let poder = "Super força";
+
+nome = "Batman"
+poder = "Rico"
+
+console.log(
+  `Nome: ${nome}
+   Idade: ${idade}
+   Poder: ${poder}
+  `
+);
