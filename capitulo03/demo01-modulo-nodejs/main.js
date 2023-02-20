806b96cc4b7644a83e24fc88ca096f33b89037a9
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-20 08:31:47 -0300

    Aula : Modulos no Node.js

---
+++ capitulo03/demo01-modulo-nodejs/main.js
@@ -0,0 +1,23 @@
+const Matematica = require('./matematica');
+
+const readline = require('readline');
+const terminal = readline.createInterface({
+  // definir o modo de entrada via terminal
+  input: process.stdin,
+  // tudo que for saida sairá no terminal
+  output: process.stdout
+})
+
+terminal.question('Insira o primeiro valor\n', valor1 => {
+  terminal.question('Insira o segundo valor\n', valor2 => {
+    terminal.question('Insira a operação \n', tipoOperacao => {
+      const resultado = Matematica[tipoOperacao](
+        Number(valor1), Number(valor2)
+      )
+      console.log(
+        `A operação ${tipoOperacao} de ${valor1} e ${valor2} é ${resultado}`
+      )
+      terminal.close()
+    })
+  })
+})
